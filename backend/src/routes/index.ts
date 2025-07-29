9ca55c9f24e4767d4094a5a9969f81c7893880fd
Rafael Escala <rafaesc92@gmail.com>
2025-07-29 01:06:53 -0500

    feature: Retrieve the userinfo from auth0

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -17,11 +17,39 @@ router.get('/', jwtCheck, function (req: Request<{}, {}, {}>, res: Response, nex
   });
 });
 
-router.get('/profile', jwtCheck, function (req: Request<{}, {}, {}>, res: Response, next: NextFunction) {
-  res.json({
-    userProfile: "NAme",
-    title: 'Profile page'
-  });
+interface UserInfo {
+  sub: string;
+  given_name: string;
+  family_name: string;
+  nickname: string;
+  name: string;
+  picture: string;
+  updated_at: string;
+  email: string;
+  email_verified: boolean;
+  permissions: string[];
+}
+
+router.get('/profile', jwtCheck, async function (req: Request<{}, {}, {}>, res: Response, next: NextFunction) {
+
+  let userInfoEndpoint = (req.auth as any)?.payload?.aud?.find((aud: any) => aud.endsWith("/userinfo"));
+
+  let userInfo = await fetch(userInfoEndpoint, ({
+    headers: {
+      Authorization: req.headers.authorization,
+    },
+  }) as any).then((res) => res.json());
+
+  if (!userInfo) {
+    res.status(401).send("Unauthorized: missing userinfo");
+    return;
+  }
+
+  userInfo.permissions = req.auth?.payload?.permissions || [];
+
+
+  res.status(200).send(userInfo as UserInfo);
+
 });
 
 export default router;
