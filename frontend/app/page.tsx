e554a3ce744cca64e1e6738ddc09cd637cefe2a4
Rafael Escala <rafaesc92@gmail.com>
2025-07-30 23:41:07 -0500

    feature: Create docker-compose

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <h1 className="text-2xl font-bold text-blue-900">BrainFuel</h1>
+                <h1 className="text-2xl font-bold text-blue-900">Quizmaster</h1>
               </div>
             </div>
             <div className="hidden md:flex items-center space-x-4">
