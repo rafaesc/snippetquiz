56d913555d8091cec46d8d43c244d39e86adc5fc
Rafael Escala <rafaesc92@gmail.com>
2025-08-05 00:43:21 -0500

    feat: connect landing to dashboard

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
