e30c3d89e635521e52748ff96ec7061a14bcc285
Rafael Escala <rafaesc92@gmail.com>
2025-10-27 00:54:27 -0500

    fix: Question Mapper issue

--- backend/java/src/main/java/ai/snippetquiz/core_service/question/adapter/out/mapper/QuestionOptionMapper.java
+++ backend/java/src/main/java/ai/snippetquiz/core_service/question/adapter/out/mapper/QuestionOptionMapper.java
@@ -1,6 +1,8 @@
 package ai.snippetquiz.core_service.question.adapter.out.mapper;
 
+import ai.snippetquiz.core_service.question.adapter.out.entities.QuestionEntity;
 import ai.snippetquiz.core_service.question.adapter.out.entities.QuestionOptionEntity;
+import ai.snippetquiz.core_service.question.domain.Question;
 import ai.snippetquiz.core_service.question.domain.QuestionOption;
 
 import java.util.List;
@@ -9,10 +11,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = {QuestionMapper.class})
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface QuestionOptionMapper {
     @Mapping(target = "question", ignore = true)
     QuestionOption toDomain(QuestionOptionEntity entity);
 
     List<QuestionOptionEntity> toEntity(List<QuestionOption> domain);
+    
+    @Mapping(target = "contentEntryId", source = "domain.contentEntryId.value")
+    @Mapping(target = "questionOptions", ignore = true)
+    QuestionEntity toEntity(Question domain);
 }
