bbbf3b029241949a954832f508e72aceba72d2e3
Rafael Escala <rafaesc92@gmail.com>
2025-10-28 02:30:21 -0500

    feat: Remove Debezium

---
+++ infra/postgres/init/211020252038__baseline.sql
@@ -0,0 +1,50 @@
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'app_user') THEN
+      CREATE ROLE app_user WITH LOGIN PASSWORD 'app_pass';
+   END IF;
+
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'readonly') THEN
+      CREATE ROLE readonly NOLOGIN;
+   END IF;
+END
+$$;
+
+DO $$
+BEGIN
+   IF NOT EXISTS (
+      SELECT 1 FROM pg_namespace WHERE nspname = 'auth'
+   ) THEN
+      EXECUTE 'CREATE SCHEMA auth';
+   END IF;
+END
+$$;
+
+DO $$
+BEGIN
+   IF NOT EXISTS (
+      SELECT 1 FROM pg_namespace WHERE nspname = 'core'
+   ) THEN
+      EXECUTE 'CREATE SCHEMA core';
+   END IF;
+END
+$$;
+
+GRANT CONNECT ON DATABASE devdb TO app_user;
+GRANT USAGE ON SCHEMA core TO app_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA core GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_user;
+
+GRANT USAGE ON SCHEMA core TO readonly;
+ALTER DEFAULT PRIVILEGES IN SCHEMA core GRANT SELECT ON TABLES TO readonly;
+
+GRANT USAGE ON SCHEMA auth TO app_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_user;
+
+GRANT USAGE ON SCHEMA auth TO readonly;
+ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT SELECT ON TABLES TO readonly;
+
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements";
+
+RAISE NOTICE 'Initiailize schema core and auth.';
