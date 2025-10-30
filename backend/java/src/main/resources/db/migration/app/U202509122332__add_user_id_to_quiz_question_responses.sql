4d4d428c5a26b7295ba3b0024b5041abd87ab237
Rafael Escala <rafaesc92@gmail.com>
2025-10-30 00:42:22 -0500

    feat: Create a single file queries migration

--- backend/java/src/main/resources/db/migration/app/U202509122332__add_user_id_to_quiz_question_responses.sql
+++
@@ -1,6 +0,0 @@
--- Drop the index
-DROP INDEX IF EXISTS "idx_quiz_question_responses_user_id_quiz_id";
-
--- Drop the user_id column
-ALTER TABLE "quiz_question_responses" 
-DROP COLUMN IF EXISTS "user_id";
