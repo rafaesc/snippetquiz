d8353a22c212b292f75af794ca5bdbbc85a38ebe
Rafael Escala <rafaesc92@gmail.com>
2026-05-11 20:36:06 -0500

    feat: Update favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,6 +11,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SnippetQuiz",
   description: "SnippetQuiz is a platform for creating and playing quizzes",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
